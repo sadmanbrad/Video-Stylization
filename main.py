2131020e59aebffdb187a43475ee1cc7701c7179
Bardia Eghbali <bardia.eghbali@gmail.com>
2022-01-05 19:08:08 +0330

    add train and test functionality to main.py

--- main.py
+++ main.py
@@ -1,12 +1,62 @@
+import os
+
+import tensorflow as tf
+
+import data_providers
 import models
+import trainers
+from tensorflow import keras
 from tensorflow.keras.utils import plot_model
 import matplotlib.pyplot as plt
+import sys
+import PIL
 
-def main():
+
+def test():
+    generator = keras.models.load_model("generator")
+    paths = sorted(os.listdir(sys.argv[2]))
+
+    images = []
+    for p in paths:
+        image = PIL.Image.open(os.path.join(sys.argv[2], p))
+        image = keras.preprocessing.image.img_to_array(image)
+        image = image[:, :, 0:3] / 255.0
+        images.append(image)
+    for i, image in enumerate(images):
+        generated = generator(image.reshape((1,) + image.shape))
+        keras.utils.save_img(f'generated/{i:04}.png', generated[0], data_format='channels_last')
+    pass
+
+
+def train():
+    regularizer = tf.keras.regularizers.l2(0.00001)
+    decay_attributes = ['kernel_regularizer', 'bias_regularizer',
+                        'beta_regularizer', 'gamma_regularizer']
+
     generator = models.make_generator()
-    plot_model(generator, "model.png", show_shapes=True, show_layer_names=True)
-    plt.show()
+    discriminator = models.make_discriminator()
+    for layer in generator.layers:
+        for attr in decay_attributes:
+            if hasattr(layer, attr):
+                setattr(layer, attr, regularizer)
+    for layer in discriminator.layers:
+        for attr in decay_attributes:
+            if hasattr(layer, attr):
+                setattr(layer, attr, regularizer)
+    perception_loss_model = models.make_perception_loss_model([0, 3, 5])
+    generator_optimizer = keras.optimizers.Adam(learning_rate=0.0004)
+    discriminator_optimizer = keras.optimizers.Adam(learning_rate=0.0004)
+    train_dataset = data_providers.PatchedDataProvider(sys.argv[2], sys.argv[3], 32)
+    data_provider = data_providers.BatchProvider(train_dataset, batch_size=40)
+
+    trainer = trainers.Trainer(generator_optimizer, discriminator_optimizer, data_provider,
+                               perception_loss_model)
+
+    trainer.train(generator, discriminator, 1)
 
 
 if __name__ == '__main__':
-    main()
+    if sys.argv[1] == 'train':
+        train()
+    if sys.argv[1] == 'test':
+        test()
